df3c94e8a8294521863e1d0de66240059262cfdc
unknown <kylelarsen12@gmail.com>
2022-03-14 19:11:40 -0400

    Styled using JS

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
+let header = document.querySelector("#page-header");
+header.style.textAlign = "left";
 
-// Select some elements...
+let dogImages = document.querySelectorAll(".dog-image");
+for (let i = 0; i < dogImages.length; i++) {
+  dogImages[i].style.borderRadius = "50px";
+  dogImages[i].style.transform = "rotateX(180deg)";
+}
+
+let names = document.querySelectorAll(".dog-name");
+for (let i = 0; i < names.length; i++) {
+  names[i].style.textAlign = "left";
+}
+
+let footerText = document.querySelectorAll(".dog-description");
+for (let i = 0; i < footerText.length; i++) {
+  footerText[i].style.color = "Purple";
+}
